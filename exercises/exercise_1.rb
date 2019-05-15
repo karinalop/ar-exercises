25bcb8bfdcfffeed591d708b865e6c1f3f1bc669
karinalop <karina.popeye@gmail.com>
2019-05-15 17:26:09 +0000

    Add validations

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -7,11 +7,17 @@ puts "----------"
 
 
 class Store < ActiveRecord::Base
-  has_many :employees
+  has_many :Employee
+  validates :name, length: { minimum: 3 }
+  validates :annual_revenue, numericality: { only_integer: true, greater_than_or_equal_to: 0}
 end
 
 class Employee < ActiveRecord::Base
-  belongs_to :store
+  belongs_to :Store
+  validates_associated :Store
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :hourly_rate, numericality: { only_integer: true, greater_than_or_equal_to: 40, less_than_or_equal_to: 200 }
 end
 
 burnaby = Store.new(name: 'Burnaby', annual_revenue: 300000, mens_apparel: true, womens_apparel: true)
